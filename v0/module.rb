462e8026e12c786479281da117279f70839c5e90
Lachlan Douglas <lachlan@engines.org>
2017-12-06 18:05:45 +1100

    test_kerberos

--- v0/module.rb
+++ v0/module.rb
@@ -111,7 +111,7 @@ class V0 < Sinatra::Base
     end
 
     begin
-      KerberosAuthenticator.authenticate!('admin@engines.dev', 'password')
+      KerberosAuthenticator.authenticate!('admin', 'password')
       out[:result] = 'Successful authentication!'.to_json
     rescue KerberosAuthenticator::Error => e
       out[:result] = "Failed to authenticate! #{e.inspect}".to_json
