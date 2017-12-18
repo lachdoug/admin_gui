425b86cbc3d38cd5b0fcf5983a2751a3d6425c43
Lachlan Douglas <lachlan@engines.org>
2017-12-18 19:24:08 +1100

    kerberos test

--- v0/api/models/user.rb
+++ v0/api/models/user.rb
@@ -25,9 +25,9 @@ class V0
     result2 =  opts[:skip_timeout]
     result3 =  check_timeout
 
-    result1 && ( result2 || result3 )
+   result =  result1 && ( result2 || result3 )
 
-          puts "auth result: " + result1.to_s + result2.to_s + result3.to_s
+          puts "auth result: " + result.to_s + ' ' + result1.to_s + ' ' + result2.to_s + ' ' + result3.to_s
           result
         end
 
