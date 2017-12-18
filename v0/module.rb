63f6e427d27f4e655bbb79a770dc693d2e33803a
Lachlan Douglas <lachlan@engines.org>
2017-12-18 19:11:04 +1100

    kerberos test

--- v0/module.rb
+++ v0/module.rb
@@ -14,7 +14,7 @@ class V0 < Sinatra::Base
   ##----------------------------------------------------------------------------
 
   before do
-    if Sinatra::Base.development?
+    if true|| Sinatra::Base.development?
       puts 'Request'
       puts request.path_info
       puts params.inspect
