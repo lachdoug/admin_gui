106c5421e96e8ccb0d313fd2b2a3cf95d350ba4c
Lachlan Douglas <lachlan@engines.org>
2019-02-15 18:45:25 +1100

    work on subservices

--- v0/api/services/engines_api.rb
+++ v0/api/services/engines_api.rb
@@ -74,9 +74,9 @@ class V0
           handle_response do
             RestClient::Request.execute(
               method: :get,
-              # url: "#{ @url }/v0/#{ route }?#{ URI.encode_www_form data }",
+              url: "#{ @url }/v0/#{ route }?#{ URI.encode_www_form data }",
               url: "#{ @url }/v0/#{ route }",
-              payload: data ? { api_vars: data }.to_json : null,
+              payload: { api_vars: ( data || {} ) }.to_json, # data ? { api_vars: data } : null,
               timeout: opts[:timeout] || 120,
               verify_ssl: false,
               headers: {
