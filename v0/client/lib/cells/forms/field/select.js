b7739b25afbfdb2724227cbbfe75b72452bd662b
Lachlan Douglas <lachlan@engines.org>
2019-04-30 14:04:24 +1000

    fix required false for select field

--- v0/client/lib/cells/forms/field/select.js
+++ v0/client/lib/cells/forms/field/select.js
@@ -7,17 +7,20 @@ var formFieldSelect = function( args ) {
 
 var formFieldSelectUnwrapped = function( args ) {
 	return {
-		$type: "select",
-		class: ( args.class || "" ) + " form-control",
-		name: ( args.name || "" ),
-		id: ( args.id || "" ),
-		placeholder: ( args.placeholder || null ),
-		required: ( args.required || false ),
-		$components: formFieldCollectionSelectOptions( args ),
-		onchange: function(e) {
-			if ( args.onchange ) {
-				return args.onchange(e)
-			};
+		...{
+			$type: "select",
+			class: ( args.class || "" ) + " form-control",
+			name: ( args.name || "" ),
+			id: ( args.id || "" ),
+			placeholder: ( args.placeholder || null ),
+			$components: formFieldCollectionSelectOptions( args ),
+			onchange: function(e) {
+				if ( args.onchange ) {
+					return args.onchange(e)
+				};
+			},
 		},
-	};
+		...args.required ? { required: 'required' } : {},
+		...args.disabled ? { disabled: 'disabled' } : {},
+	}
 };
